a2c2e06c4c5ec026e9ec886d44d9930cbd02b796
coder-chekunkov <chekunkov-work@yandex.ru>
2026-01-28 22:07:06 +0300

    Added a biometry authorization (iOS) - part 2

--- core/core-authentication/src/iosMain/kotlin/com/chknkv/coreauthentication/utils/BiometricContextProvider.ios.kt
+++ core/core-authentication/src/iosMain/kotlin/com/chknkv/coreauthentication/utils/BiometricContextProvider.ios.kt
@@ -1,12 +1,28 @@
 package com.chknkv.coreauthentication.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.chknkv.coreauthentication.models.domain.BiometricContext
+import platform.UIKit.UIApplication
 
 /**
- * iOS actual: placeholder; returns null. Biometric auth is not yet implemented on iOS.
+ * iOS actual: returns BiometricContext with the current UIViewController.
+ * Gets the root view controller from the key window.
  */
 @Composable
 internal actual fun getBiometricContext(): BiometricContext? {
-    return null
+    return remember {
+        try {
+            val application = UIApplication.sharedApplication
+            val keyWindow = application.keyWindow
+            val rootViewController = keyWindow?.rootViewController
+            if (rootViewController != null) {
+                BiometricContext(rootViewController)
+            } else {
+                null
+            }
+        } catch (_: Exception) {
+            null
+        }
+    }
 }
